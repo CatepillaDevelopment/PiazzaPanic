2bbf7ba61084ec362b256c7f0e6e2272e1c04182
Jack Hardy <118314331+jjh599@users.noreply.github.com>
2023-01-18 19:22:26 +0000

    Added ruined to ItemBase
    All Item based entities now have the attribute ruined and related methods.

--- src/piazzapanic/entitiysystem/dynamic/items/ItemBase.java
+++ src/piazzapanic/entitiysystem/dynamic/items/ItemBase.java
@@ -3,5 +3,23 @@ package piazzapanic.entitiysystem.dynamic.items;
 import piazzapanic.entitiysystem.dynamic.DynamicObjectBase;
 
 public abstract class ItemBase extends DynamicObjectBase {
+    protected boolean ruined;
     
+    public ItemBase(){
+        this.ruined = false;
+    }
+
+    public boolean isRuined(){
+        return this.ruined;
+    }
+
+    public boolean ruin(){
+        if (this.ruined == false){
+            this.ruined = true;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
