3171808b39a16228ce21c6c8daf45b6cf60806fe
Benji-Young <benjaminty2003@gmail.com>
2023-01-30 13:25:58 +0000

    Draw Ingredient Stations
    draw the ingredient stations to the map

---
+++ Game/assets/gameLevel/IngredientStations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="IngredientStations" tilewidth="200" tileheight="275" tilecount="6" columns="3">
+ <image source="../GameSpritesheets/ingredientStations#.png" width="600" height="550"/>
+</tileset>
