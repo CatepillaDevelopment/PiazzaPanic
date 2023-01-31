2dddb6815897e4ef88f23d304ac46449c948f7a0
bpu500 <bpu500@york.ac.uk>
2023-01-31 22:29:30 +0000

    remove WASD variables from main

--- Game/core/src/piazzapanic/core/Main.java
+++ Game/core/src/piazzapanic/core/Main.java
@@ -9,10 +9,6 @@ public class Main extends Game {
 	public SpriteBatch batch;
 	public static int width = 1600;
 	public static int height = 1200;
-	boolean movingUp = false;
-	boolean movingDown = false;
-	boolean movingLeft = false;
-	boolean movingRight = false;
 	public MenuScreen menu;
 
 	@Override
