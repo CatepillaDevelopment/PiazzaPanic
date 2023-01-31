df0c6c1856262fc673730706db3670605bdbda55
bpu500 <bpu500@york.ac.uk>
2023-01-31 22:28:39 +0000

    Remove redundant WASD control from main

--- Game/core/src/piazzapanic/core/Main.java
+++ Game/core/src/piazzapanic/core/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
 
-public class Main extends Game implements InputProcessor {
+public class Main extends Game {
 	public SpriteBatch batch;
 	public static int width = 1600;
 	public static int height = 1200;
@@ -30,53 +30,5 @@ public class Main extends Game implements InputProcessor {
 	@Override
 	public void dispose () {
 		batch.dispose();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		if (keycode == Input.Keys.W) movingUp = true;
-		if (keycode == Input.Keys.S) movingDown = true;
-		if (keycode == Input.Keys.A) movingLeft = true;
-		if (keycode == Input.Keys.D) movingRight = true;
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		if (keycode == Input.Keys.W) movingUp = false;
-		if (keycode == Input.Keys.S) movingDown = false;
-		if (keycode == Input.Keys.A) movingLeft = false;
-		if (keycode == Input.Keys.D) movingRight = false;
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		return false;
 	}
 }
